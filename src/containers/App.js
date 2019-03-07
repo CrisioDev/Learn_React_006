b8fc0fd6312c1057ce4b28a2d07466571e8dcbc6
CrisioDev <crisioletsplay@gmail.com>
2019-03-08 00:21:09 +0100

    Lifecycle-Hooks added for better understanding

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state  = {
     persons: [
       {id:'170364', name: 'Christopher',age:'23'},
@@ -12,6 +17,20 @@ class App extends Component {
       {id:'871642', name: 'Calvin',age:'20'}
     ],
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
+
+  //rarely used - sooner or later will be removed
+  componentWillMount(){
+    console.log('[App.js] componentWillMount')
   }
 
   deletePersonHandler = (personIndex) => {
@@ -43,6 +62,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if(this.state.showPersons){
