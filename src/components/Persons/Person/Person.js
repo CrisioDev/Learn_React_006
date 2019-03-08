48dd093561f7086768164a0c80a79f8bc418d458
CrisioDev <crisioletsplay@gmail.com>
2019-03-08 02:27:56 +0100

    Using Aux-Component for replacing root JSX-Div

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux' //import higher order component
 import styles from './Person.css';
 
 class Person extends Component {
     render(){
         console.log('[Person.js rendering...]');
         return (
-            <div className={styles.Person}>
+            <Aux> 
                 <p onClick={this.props.click}>#{this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
-        ); 
+            </Aux>
+        ); //wrapping Aux allows to get multiple JSX-Elements in return without div wrapping 
     }
 };
 
